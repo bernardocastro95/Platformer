447020ad3dc858e68f789c69e0fa4ea8b674ae77
Bernardo Castro <bernardoc544@gmail.com>
2021-05-07 18:20:59 -0400

    Invisible Laser Beam

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
     private float _gravity = 9.81f;
+    [SerializeField]
+    private ParticleSystem _shot;
 
     // Start is called before the first frame update
     void Start()
@@ -16,26 +18,36 @@ public class PlayerController : MonoBehaviour
         _controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        _shot = GameObject.Find("Muzzle_Flash").GetComponentInParent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Cursor.visible == false && Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
+        
+
         if (Input.GetMouseButtonDown(0))
         {
+            
             Ray origin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hit;
+            _shot.Play(true);
 
             if (Physics.Raycast(origin, out hit))
             {
                 Debug.Log("HIT: " + hit.transform.name);
             }
+        }
+        else
+        {
+            _shot.Stop(true);
+        }
+
+        if (Cursor.visible == false && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
         MovementPlayer();
 
